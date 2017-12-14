aa883f54cb40f2810d511c38b126430345422cf5
maxcheshier <maxcheshier@gmail.com>
2017-12-14 13:31:41 -0800

    Update MP3.cpp

--- L5_Application/MP3.cpp
+++ L5_Application/MP3.cpp
@@ -11,14 +11,42 @@
 #include "storage.hpp"
 #include "command_handler.hpp"
 #include "ssp0.h"
-
+#include "LCD.hpp"
+#include "uart3.hpp"
+#include "Decoder.hpp"
 
 
 GPIO dreqPin(P1_19);
 GPIO XCS(P1_22);
 GPIO XDCS(P1_30);
 GPIO decodeRST(P1_28);
+GPIO playOrPausePin(P2_0);
+GPIO nextSong(P2_1);
+int counter = 0;
+bool paused = false;
 
+const char *songNames[]{
+	"1:01 Keep the Family Close.mp3",
+	"1:02 9.mp3",
+	"1:03 U With Me.mp3",
+	"1:04 Feel No Ways.mp3",
+	"1:05 Hype.mp3",
+	"1:06 Weston Road Flows.mp3",
+	"1:07 Redemption.mp3",
+	"1:08 With You.mp3",
+	"1:09 Faithful.mp3",
+	"1:10 Still Here.mp3",
+	"1:11 Controlla.mp3",
+	"1:12 One Dance.mp3",
+	"1:13 Grammys.mp3",
+	"1:14 Childs Play.mp3",
+	"1:15 Pop Style.mp3",
+	"1:16 Too Good.mp3",
+	"1:17 Summers Over Interlude.mp3",
+	"1:18 Fire & Desire.mp3",
+	"1:19 Views.mp3",
+	"1:20 Hotline Bling.mp3"
+};
 
 
 bool MP3::init(){
@@ -33,31 +61,43 @@ bool MP3::init(){
     decodeRST.setLow();
     decodeRST.setHigh();
 
-
+    
+ //    Uart3& u3 = Uart3::getInstance();
+	// u3.init(38400,50,50);
 
 	return true;
 }
 	
 bool MP3::run(void *param){
+	Uart3& u3 = Uart3::getInstance();
+	u3.init(38400,50,50);
+	u3.putline(songNames[counter]);
+
 	readAndSend();
 	return true;
 }
 
 void MP3::readAndSend(){
-	XCS.setLow();
-	char data [512] = {0};
-	int i = 0;
-	while(1){
-		Storage::read("1:Jaden Smith - Icon.mp3", data, sizeof(data)-1, i);
-		i += 512;
-		for(int j =0; j< 16; j++){
-			while(!dreqPin.read());
-			XDCS.setLow();
-			for(int k=0; k<32; k++){
-					ssp0_exchange_byte(data[k+(j*32)]);
+		decoderRegister decoderObject;
+		XCS.setLow();
+		char data [512] = {0};
+		int i = 0;
+		while(1){
+				
+				Storage::read(songNames[counter], data, sizeof(data)-1, i);
+				i += 512;
+				for(int j =0; j< 16; j++){
+					if(nextSong.read()){
+						counter++;
+						break;
+					}
+					while(!dreqPin.read());
+					XDCS.setLow();
+					for(int k=0; k<32; k++){
+							ssp0_exchange_byte(data[k+(j*32)]);
+						}
+					XDCS.setHigh();
 				}
-			XDCS.setHigh();
+			}
+			XCS.setHigh();
 		}
-	}
-	XCS.setHigh();
-}
